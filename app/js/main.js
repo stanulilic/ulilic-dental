6b63dd6f503614facc2e397f659c2dc6139989e5
Stanley Ulili <stanleyulili@gmail.com>
2019-07-13 15:37:48 +0200

    finished styling navigation for both mobile and desktop

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,21 @@
 
 var hamburger = document.getElementById("hamburger");
 var nav = document.getElementById("navigation");
+var closeIcon = document.getElementById('close-nav-icon');
+var openIcon = document.getElementById('open-nav-icon');
 
 hamburger.addEventListener('click', function(){
     if (nav.classList.contains('hide')){
         nav.classList.remove('hide');
+        openIcon.classList.add('hide_element');
+        closeIcon.classList.remove('hide_element');
         nav.classList.add('show');
     }
 
     else if(nav.classList.contains('show')){
         nav.classList.remove('show');
         nav.classList.add('hide');
+        openIcon.classList.remove('hide_element');
+        closeIcon.classList.add('hide_element');
     }
 })
