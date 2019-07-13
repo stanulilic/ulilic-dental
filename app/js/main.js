30bd4519f0e497b69d7f846c2ac40d50eed48f84
Stanley Ulili <stanleyulili@gmail.com>
2019-07-13 12:46:13 +0200

    finished mobile navigation

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,15 @@
 
+var hamburger = document.getElementById("hamburger");
+var nav = document.getElementById("navigation");
+
+hamburger.addEventListener('click', function(){
+    if (nav.classList.contains('hide')){
+        nav.classList.remove('hide');
+        nav.classList.add('show');
+    }
+
+    else if(nav.classList.contains('show')){
+        nav.classList.remove('show');
+        nav.classList.add('hide');
+    }
+})
